7001121e22a48b2f71d00c3f743e9e0b2ec47876
Ricardo Andrade <ricardo.maia@ccc.ufcg.edu.br>
2021-08-28 22:00:09 -0300

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('ovo funcionar'),
+    };
+    console.log("oporra, deu certa!");
+    return response;
+};
